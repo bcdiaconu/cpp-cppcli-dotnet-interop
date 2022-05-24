b402e446ee855cd666fd66bf97194792e781feab
cosmin.diaconu <cosmin.diaconu@kla-tencor.com>
2022-05-24 14:10:23 +0300

    create maping functions

--- CppCliWrapper/CalculatorWrapper.cpp
+++ CppCliWrapper/CalculatorWrapper.cpp
@@ -4,7 +4,35 @@
 
 class CalculatorPimpl
 {
-   gcroot<CsLibrary::Calculator^> _calculator;
+private:
+    gcroot<CsLibrary::Calculator^> _calculator;
+
+    static System::String^ mapStdToSystemString(const std::string& std_string)
+    {
+        return gcnew System::String(std_string.c_str());
+    }
+
+    static std::string mapSystemToStdString(System::String^ sys_string)
+    {
+        using namespace System::Runtime::InteropServices;
+        auto chars = (const char*) (Marshal::StringToHGlobalAnsi(sys_string)).ToPointer();
+        std::string os = chars;
+        Marshal::FreeHGlobal(System::IntPtr((void*) chars));
+
+        return os;
+    }
+
+    static std::wstring mapSystemToWstring(System::String^ sys_string)
+    {
+
+        using namespace System::Runtime::InteropServices;
+        auto chars = (const wchar_t*) (Marshal::StringToHGlobalUni(sys_string)).ToPointer();
+        std::wstring os = chars;
+        Marshal::FreeHGlobal(System::IntPtr((void*) chars));
+
+        return os;
+    }
+
 public:
    CalculatorPimpl(double fpNum1, double fpNum2)
       :_calculator(gcnew CsLibrary::Calculator(fpNum1, fpNum2))
